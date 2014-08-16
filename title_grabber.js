8efa577572072603284a406e8d22b2590656a26f
glepur <goran.lepur1@gmail.com>
2014-08-16 18:15:07 +0200

    Fixed spaces in titles

--- title_grabber.js
+++ title_grabber.js
@@ -1,7 +1,7 @@
 var h1 = document.getElementsByTagName('h1');
 
 if (h1.length) {
-	var title = h1[0].innerHTML.split('Episode');
+	var title = h1[0].innerHTML.split(' Episode ');
 	if (title.length > 1) {
 		title;
 	} else {
